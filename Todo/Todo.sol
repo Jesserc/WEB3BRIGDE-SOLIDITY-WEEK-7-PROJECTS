ff55f77b6f8812c2e7e0e756f5680a76c75921db
Jesse Raymond <94083757+Jesserc@users.noreply.github.com>
2022-08-25 11:06:03 +0100

    added modifier to deleteTask() function

--- Todo/Todo.sol
+++ Todo/Todo.sol
@@ -52,7 +52,7 @@ contract TodoContract {
         return todos;
     }
 
-    function deleteTask(uint256 index) external {
+    function deleteTask(uint256 index) external todoExists(index) {
         for (uint256 i = index; i < todos.length - 1; i++) {
             todos[i] = todos[i + 1];
         }
